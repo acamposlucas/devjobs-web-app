44bedc99fea8f8863e6cc5524b2af0035ba20f18
lucasalmeida <lac.lucas96@gmail.com>
2023-06-28 20:55:33 -0300

    feat: create search filter component

--- src/app/public/shared/header/header.component.ts
+++ src/app/public/shared/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchFilterComponent } from '../components/search-filter/search-filter.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchFilterComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })

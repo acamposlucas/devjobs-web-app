4a7ddaa64b39065add1c965fb9b8958a3ca039da
lucasalmeida <lac.lucas96@gmail.com>
2023-06-28 18:27:51 -0300

    refactor!: bootstrapping app using standalone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	imports: [RouterModule],
+	template: "<router-outlet></router-outlet>",
+	standalone: true,
 })
 export class AppComponent {
-  title = 'devjobs-web-app';
+	title = "devjobs-web-app";
 }

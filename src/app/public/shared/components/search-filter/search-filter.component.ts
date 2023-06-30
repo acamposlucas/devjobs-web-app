54e3da4f1788d5562dd3e2989bfeae5386a64b45
lucasalmeida <lac.lucas96@gmail.com>
2023-06-30 18:10:24 -0300

    feat: create dialog component

--- src/app/public/shared/components/search-filter/search-filter.component.ts
+++ src/app/public/shared/components/search-filter/search-filter.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, TemplateRef, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { DialogComponent } from "src/app/shared/components/dialog/dialog.component";
+import { ModalService } from "src/app/shared/services/modal.service";
 
 @Component({
-  selector: 'app-search-filter',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './search-filter.component.html',
-  styleUrls: ['./search-filter.component.scss']
+	selector: "app-search-filter",
+	standalone: true,
+	imports: [CommonModule, DialogComponent],
+	templateUrl: "./search-filter.component.html",
+	styleUrls: ["./search-filter.component.scss"],
 })
 export class SearchFilterComponent {
+	modalService = inject(ModalService);
 
+	showDialog(): void {
+		this.modalService.show();
+	}
 }

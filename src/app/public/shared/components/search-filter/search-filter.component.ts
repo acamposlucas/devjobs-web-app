09d871bc464c72f25134182bb6c577d4aa73c97c
lucasalmeida <lac.lucas96@gmail.com>
2023-07-01 00:01:32 -0300

    refactor: remove unused dialog component

--- src/app/public/shared/components/search-filter/search-filter.component.ts
+++ src/app/public/shared/components/search-filter/search-filter.component.ts
@@ -1,18 +1,15 @@
 import { Component, TemplateRef, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { DialogComponent } from "src/app/shared/components/dialog/dialog.component";
-import { ModalService } from "src/app/shared/services/modal.service";
 import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 
 @Component({
 	selector: "app-search-filter",
 	standalone: true,
-	imports: [CommonModule, DialogComponent, MatDialogModule],
+	imports: [CommonModule, MatDialogModule],
 	templateUrl: "./search-filter.component.html",
 	styleUrls: ["./search-filter.component.scss"],
 })
 export class SearchFilterComponent {
-	modalService = inject(ModalService);
 	matDialog = inject(MatDialog);
 
 	openDialog(content: TemplateRef<any>): void {

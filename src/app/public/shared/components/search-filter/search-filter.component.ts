45676076e6f259b5c3a6d7ecc45c60c25b5f8b4c
lucasalmeida <lac.lucas96@gmail.com>
2023-06-30 23:53:05 -0300

    feat!: add angular material. refactor: uses material dialog

--- src/app/public/shared/components/search-filter/search-filter.component.ts
+++ src/app/public/shared/components/search-filter/search-filter.component.ts
@@ -2,18 +2,22 @@ import { Component, TemplateRef, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DialogComponent } from "src/app/shared/components/dialog/dialog.component";
 import { ModalService } from "src/app/shared/services/modal.service";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
 
 @Component({
 	selector: "app-search-filter",
 	standalone: true,
-	imports: [CommonModule, DialogComponent],
+	imports: [CommonModule, DialogComponent, MatDialogModule],
 	templateUrl: "./search-filter.component.html",
 	styleUrls: ["./search-filter.component.scss"],
 })
 export class SearchFilterComponent {
 	modalService = inject(ModalService);
+	matDialog = inject(MatDialog);
 
-	showDialog(): void {
-		this.modalService.show();
+	openDialog(content: TemplateRef<any>): void {
+		let dialogRef = this.matDialog.open(content, {
+			width: "300px",
+		});
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
 import { ROUTES } from "./app/app-routing";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { importProvidersFrom } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
-	providers: [provideRouter(ROUTES)],
+	providers: [provideRouter(ROUTES), importProvidersFrom(BrowserAnimationsModule)],
 });

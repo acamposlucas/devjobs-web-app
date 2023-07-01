d1dd56fb1ed1eefdc23edc1371b2a7168025322b
lucasalmeida <lac.lucas96@gmail.com>
2023-07-01 00:45:54 -0300

    feat: create job-card component

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../shared/header/header.component';
+import { JobCardComponent } from '../../shared/components/job-card/job-card.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, JobCardComponent],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
---
+++ src/app/public/shared/components/job-card/job-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-job-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './job-card.component.html',
+  styleUrls: ['./job-card.component.scss']
+})
+export class JobCardComponent {
+
+}
